808c344936fa2a4ea7b25492614ab7a8b2d10d95
Thomas van der Velde <contact@toolbarthomas.net>
2018-07-12 22:57:19 +0200

    Keeps folder structure for builds

--- webpack.config.common.js
+++ webpack.config.common.js
@@ -1,4 +1,24 @@
+const path = require("path");
+
+const webpack_entries = {
+  raw: [`${process.env.SRC}/base/javascripts/index.js`],
+  parsed: {}
+};
+
+for (let i = 0; i < webpack_entries.raw.length; i++) {
+  let extension = path.extname(webpack_entries.raw[i]);
+  let key = webpack_entries.raw[i]
+    .replace(process.env.SRC, "")
+    .replace(extension, "");
+
+  webpack_entries.parsed[key] = webpack_entries.raw[i];
+}
+
 module.exports = {
   mode: "none",
-  entry: `${process.env.SRC}/base/javascripts/index.js`
+  entry: webpack_entries.parsed,
+  output: {
+    path: path.resolve(__dirname, process.env.DIST),
+    filename: "[name].js"
+  }
 };
