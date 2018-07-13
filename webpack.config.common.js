93ba942cfd96ad0b9bb4ae95c8b32c501da51905
Thomas van der Velde <contact@toolbarthomas.net>
2018-07-13 14:13:36 +0200

    Implements cssnext & stylelint

--- webpack.config.common.js
+++ webpack.config.common.js
@@ -1,4 +1,11 @@
+/**
+ * Global configuration file for all defined environments.
+ */
+
 const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const StyleLintPlugin = require("stylelint-webpack-plugin");
+const StyleLintFormatterPretty = require("stylelint-formatter-pretty");
 
 const webpack_entries = {
   raw: [`${process.env.SRC}/base/index.js`],
@@ -20,5 +27,39 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, process.env.DIST),
     filename: "[name].js"
+  },
+  plugins: [
+    /**
+     * Webpack will parse our initial stylesheet from the base entry file: base/index.js
+     */
+    new MiniCssExtractPlugin({
+      filename: "[name].css"
+    }),
+    new StyleLintPlugin({
+      configFile: `${process.cwd()}/.stylelintrc`,
+      formatter: StyleLintFormatterPretty,
+      context: process.env.SRC,
+      quiet: false,
+      files: "**/*.css",
+      failOnError: false,
+      emitErrors: true
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              sourceMap: true
+            }
+          },
+          "css-loader",
+          "postcss-loader"
+        ]
+      }
+    ]
   }
 };
